89b00b9ebdf995a7bfe31b9411d9931aec5b548a
Hynson <hynson@163.com>
2022-02-18 14:25:47 +0800

    原生中展示FragmentView

--- app/src/main/java/com/hynson/flutterlearn/MainActivity.java
+++ app/src/main/java/com/hynson/flutterlearn/MainActivity.java
@@ -1,24 +1,91 @@
 package com.hynson.flutterlearn;
 
-import androidx.appcompat.app.AppCompatActivity;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.view.FlutterMain;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    private static final String TAG_FLUTTER_FRAGMENT = "flutter_fragment";
+    private FlutterFragment flutterFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn_flutter).setOnClickListener(new View.OnClickListener() {
+        initFragment();
+
+        findViewById(R.id.btn_activity).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(FlutterActivity.withCachedEngine("my_engine_id").build(MainActivity.this));
             }
         });
+        findViewById(R.id.btn_fragment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .add(R.id.fl_contain, flutterFragment, TAG_FLUTTER_FRAGMENT)
+                        .commit();
+            }
+        });
+    }
+
+    private void initFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        flutterFragment = (FlutterFragment) fragmentManager
+                .findFragmentByTag(TAG_FLUTTER_FRAGMENT);
+        if (flutterFragment == null) {
+            flutterFragment = FlutterFragment.withCachedEngine("my_engine_id").build();
+        }
+    }
+
+    @Override
+    public void onPostResume() {
+        super.onPostResume();
+        flutterFragment.onPostResume();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        flutterFragment.onNewIntent(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        flutterFragment.onBackPressed();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String[] permissions,
+            int[] grantResults
+    ) {
+        flutterFragment.onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+        );
+    }
+
+    @Override
+    public void onUserLeaveHint() {
+        flutterFragment.onUserLeaveHint();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        flutterFragment.onTrimMemory(level);
     }
 }
